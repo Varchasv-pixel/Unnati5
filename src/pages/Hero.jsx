725143fbee79dc46095db462cba14ebeaafd628e
Pinak <viveksharma_240087@aitpune.edu.in>
2025-08-24 05:57:00 +0530

    font fixed

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -53,7 +53,7 @@ function Hero() {
   return (
     <div className='flex bganimation flex-col justify-center items-center gap-y-20 mt-21 pt-18 md:mt-40 md:pt-25 lg:mt-70 lg:pt-37   pb-8 ' ref={divRef} style={{borderTopLeftRadius: '100%', borderTopRightRadius: '100%', width: '85%'}}>
       <div className='flex flex-col justify-center items-center text-center text-white gap-y-8'>
-        <p className='font-family-primary font-extrabold text-[clamp(1.5rem,6.1vw,4rem)] '>The all-in-one <br /> workspace you'll love</p>
+        <p className='font-primary font-bold text-[clamp(1.5rem,6.1vw,4rem)] '>The all-in-one <br /> workspace you'll love</p>
         <p className='leading-0 text-[1rem] '>Think it. Plan it. Build it. while switching apps</p>
         
         <Primarybtn/>
